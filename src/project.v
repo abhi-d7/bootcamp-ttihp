03028c1357c583d3ef14afe005be83d2a7ee8643
abhi-d7 <abhijithdamu97.d@gmail.com>
2026-03-05 23:20:00 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_abhi-d7_bootcamp-ttihp(
+module tt_um_abhi_d7_bootcamp_ttihp(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
